bc82746e2a88b78df6b7c7c786e8a7165e6b137b
Sakaki <sakajun0208@gmail.com>
2023-04-20 06:48:40 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ConsoleOutputCapture
+{
+    class ConsoleOutputCapture
+    {
+        static void Main(string[] args)
+        {
+            // Write Excute Command
+            string env = "powershell.exe";
+            string command = "Connect-MgGraph";
+
+            // ProcessStartInfoを作成し、プロセスの設定を行います。
+            ProcessStartInfo psi = new ProcessStartInfo(env, command)
+            {
+                RedirectStandardOutput = true, // 標準出力をリダイレクトすることを指定します
+                RedirectStandardError = true,  // 標準エラーをリダイレクトすることを指定します
+                UseShellExecute = false,        // シェル機能を使用せず、プロセスを直接実行することを指定します
+                CreateNoWindow = true          // 新しいウィンドウを作成せずに実行することを指定します
+            };
+
+            // Processオブジェクトを作成し、プロセスを実行します。 
+            using (Process process = new Process { StartInfo = psi })
+            {
+                process.Start(); //プロセスを開始します
+
+                //標準出力と標準エラーを取得します
+                string standardOutput = process.StandardOutput.ReadToEnd();
+                string standardError = process.StandardError.ReadToEnd();
+
+                process.WaitForExit(); //プロセスが終了するまで待機します
+
+                // テスト
+                Console.WriteLine("標準出力");
+                Console.WriteLine(standardOutput);
+                Console.WriteLine("標準エラー");
+                Console.WriteLine(standardError);
+
+            }
+
+
+        }
+    }
+}
